a70717cdbce8ea9e3a503b38a6ffad920911f16a
Alex <ifrostgroup@gmail.com>
2016-04-06 14:32:06 +0300

    Test

---
+++ src/test/GoodBy.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * Created by Семейка плюшиков on 06.04.2016.
+ */
+public class GoodBy {
+    public static void main(String[] args) {
+        System.out.println("By");
+    }
+
+}
