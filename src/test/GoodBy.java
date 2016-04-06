45aa5cb9a7082bd480a467ea16a54ec39a25637b
Alex <ifrostgroup@gmail.com>
2016-04-06 14:18:48 +0300

    Test

--- src/test/GoodBy.java
+++ src/test/GoodBy.java
@@ -1,8 +1,10 @@
+package test;
+
 /**
  * Created by Семейка плюшиков on 06.04.2016.
  */
-public class Hello {
+public class GoodBy {
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println(" ");    }
+        System.out.println("Good by");
+    }
 }
---
+++ src/test/Hello.java
@@ -0,0 +1,10 @@
+package test;
+
+/**
+ * Created by Семейка плюшиков on 06.04.2016.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println(" ");    }
+}
